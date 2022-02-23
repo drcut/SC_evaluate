2408f74c730ffc7a9bcb6512b9f7c647e9a1200e
RobinHan <hanruobing@pku.edu.cn>
2022-02-23 16:03:12 -0500

    upload

---
+++ Hetero-cox/src/kmeans/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clang++ -std=c++11 cuda/kmeans_cuda_benchmark.cu -I../.. --cuda-path=/usr/local/cuda-10.1 \
+    --cuda-gpu-arch=sm_50 -L/usr/local/cuda-10.1/lib64 \
+    -lcudart_static -ldl -lrt -pthread -save-temps -v
+
+kernelTranslator kmeans_cuda_benchmark-cuda-nvptx64-nvidia-cuda-sm_50.bc kernel.bc
+hostTranslator kmeans_cuda_benchmark-host-x86_64-unknown-linux-gnu.bc host.bc
+
+llc --relocation-model=pic --filetype=obj  kernel.bc
+llc --relocation-model=pic --filetype=obj  host.bc
+
+g++ -o kemans -fPIC -no-pie -L/home/robinhan/repo/open_source_template/build/runtime \
+  -L/home/robinhan/repo/open_source_template/build/runtime/threadPool \
+  cuda/main.cc host.o kernel.o *.cc \
+ ../common/benchmark/*.cc ../common/command_line_option/*.cc \
+ ../common/time_measurement/*.cc -I../.. -I/home/robinhan/repo/open_source_template/runtime/include/ \
+  -I../.. -I/home/robinhan/repo/open_source_template/runtime/threadPool/include/ \
+ -lpthread -lc -lx86Runtime -lthreadPool
+
+./kmeans -i ../../data/kmeans/100_34.txt -q -v
